58f44aecdba2ce1412afd79cf2ef3dafa8c6b3f5
NikolaKrsmanovic1996 <nikolakrsmanovic223@gmail.com>
2019-12-12 16:57:33 +0100

    After 3 days i finally realized how to disable auto-security, i putted
    in application.properties single line of code in the line 22. Now i
    start backend of my application

--- Bookstore/src/main/java/com/bookstore/BookstoreApplication.java
+++ Bookstore/src/main/java/com/bookstore/BookstoreApplication.java
@@ -2,8 +2,11 @@ package com.bookstore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.bookstore.controller"} )
 public class BookstoreApplication {
 
 	public static void main(String[] args) {
