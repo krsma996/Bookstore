8c14d6c883bfe70f21c77a1bc39795e9541e8586
NikolaKrsmanovic1996 <nikolakrsmanovic223@gmail.com>
2020-01-15 21:05:17 +0100

    Added new Features
    Added dependecy for email,springsecurity4,spring-context-support
    Defined in propery file
    UserRepository defined username and email
    UserService defined sets for new Users
    UserServiceIml
    HomeController autowired mailSender,MailConstrutctor and defined newUser
    and his role

--- Bookstore/src/main/java/com/bookstore/BookstoreApplication.java
+++ Bookstore/src/main/java/com/bookstore/BookstoreApplication.java
@@ -1,19 +1,53 @@
 package com.bookstore;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
+import com.bookstore.domain.User;
+import com.bookstore.domain.security.Role;
+import com.bookstore.domain.security.UserRole;
+import com.bookstore.service.UserService;
+import com.bookstore.utility.SecurityUtility;
+
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.bookstore.controller", "com.bookstore",} )
 @Configuration
 @EnableAutoConfiguration
-public class BookstoreApplication {
+public class BookstoreApplication implements CommandLineRunner {
 
+	@Autowired
+	private UserService userService;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(BookstoreApplication.class, args);
 	}
+	
+	@Override
+		public void run(String... args) throws Exception{
+		    User user1 = new User();
+		    user1.setFirstName("Nikola");
+		    user1.setLastName("Krsmanovic");
+		    user1.setUsername("n");
+		    user1.setPassword(SecurityUtility.passwordEncoder().encode("p"));
+		    user1.setEmail("nikolakrsmanovic223@gmail.com");
+		    Set<UserRole> userRoles = new HashSet<>();
+		    Role role1 = new Role();
+		    role1.setRoleId(1);
+		    role1.setName("ROLE_USER");
+		    userRoles.add(new UserRole(user1,role1));
+		    
+		    
+		    userService.createUser(user1, userRoles);
+	}
+	
+	
 }
--- Bookstore/src/main/java/com/bookstore/service/impl/UserServiceImpl.java
+++ Bookstore/src/main/java/com/bookstore/service/impl/UserServiceImpl.java
@@ -1,15 +1,27 @@
 package com.bookstore.service.impl;
 
+import java.util.Set;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.bookstore.domain.User;
 import com.bookstore.domain.security.PasswordResetToken;
+import com.bookstore.domain.security.UserRole;
 import com.bookstore.repository.PasswordResetTokenRepository;
+import com.bookstore.repository.RoleRepository;
+import com.bookstore.repository.UserRepository;
 import com.bookstore.service.UserService;
 
 @Service
 public class UserServiceImpl implements UserService{
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Autowired
+	private RoleRepository roleRepository;
+	
 	
 	@Autowired
 	private PasswordResetTokenRepository passwordResetTokenRepository;
@@ -24,6 +36,34 @@ public class UserServiceImpl implements UserService{
 		final PasswordResetToken myToken = new PasswordResetToken(token, user);
 		passwordResetTokenRepository.save(myToken);
 	}
-
+	
+	
+	@Override
+	public User findByUsername(String username) {
+		return userRepository.findByUsername(username);
+	}
+	@Override
+	public User findByEmail(String email) {
+		return userRepository.findByEmail(email);
+		
+	}
+	 @Override
+      public User createUser(User user, Set<UserRole> userRoles) throws Exception {
+    	   User localUser = userRepository.findByUsername(user.getUsername());
+    	   
+    	   if(localUser !=null) {
+    		   throw new Exception("user alredy exsist. Nothing will be done.");
+    	   }else {
+    		   for(UserRole ur: userRoles) {
+    			   roleRepository.save(ur.getRole());
+    		   }
+    		   
+    		   user.getUserRoles().addAll(userRoles);
+    		   
+    		   localUser = userRepository.save(user);
+    	   }
+    	   
+    	   return localUser;
+      }
 }
 
