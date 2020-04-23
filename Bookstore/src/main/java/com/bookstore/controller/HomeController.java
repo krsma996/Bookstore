eed7ecd17d5f9a822d12fd4bfd53819342ccbc4d
NikolaKrsmanovic1996 <nikolakrsmanovic223@gmail.com>
2020-04-23 13:37:51 +0200

    ADDED new Features
    New property file messeages.properties for adminPath
    
    THen again created bookshelf.Html created table and added in td span
    rows after that on seaction-headline on top added th:text for user if
    user is not founded then return null and if is found return all books
    
    Copied class book from adminportal changed packeges
    Created BookRepostiory interface,BookService Interface,BookServiceImpl
    class and defined List of books methods and in bookRepository
    implemented interface CrudReposiory for Book
    
    HomeController Autowired bookService
    and defined view RequestMapping

--- Bookstore/src/main/java/com/bookstore/controller/HomeController.java
+++ Bookstore/src/main/java/com/bookstore/controller/HomeController.java
@@ -1,6 +1,8 @@
 package com.bookstore.controller;
 
+
 import java.util.HashSet;
+import java.util.List;
 import java.util.Locale;
 import java.util.Set;
 import java.util.UUID;
@@ -21,10 +23,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.bookstore.domain.Book;
 import com.bookstore.domain.User;
 import com.bookstore.domain.security.PasswordResetToken;
 import com.bookstore.domain.security.Role;
 import com.bookstore.domain.security.UserRole;
+import com.bookstore.service.BookService;
 import com.bookstore.service.UserService;
 import com.bookstore.service.impl.UserSecurityService;
 import com.bookstore.utility.MailConstructor;
@@ -45,6 +49,9 @@ public class HomeController {
 	@Autowired
 	private UserSecurityService userSecurityService;
 
+	@Autowired
+	private BookService bookService;
+	
 	@RequestMapping("/")
 	public String index() {
 		return "index";
@@ -55,6 +62,17 @@ public class HomeController {
 		model.addAttribute("classActiveLogin", true);
 		return "myAccount";
 	}
+	
+	@RequestMapping("/bookshelf")
+	public String bookshelf(Model model) {
+		List<Book> bookList = bookService.findAll();
+		model.addAttribute("bookList", bookList);
+		
+		return "bookshelf";
+	}
+	
+	
+	
 
 	@RequestMapping("/forgetPassword")
 	public String forgetPassword(
---
+++ Bookstore/src/main/java/com/bookstore/service/BookService.java
@@ -0,0 +1,13 @@
+package com.bookstore.service;
+
+
+import java.util.List;
+
+import com.bookstore.domain.Book;
+
+public interface BookService {
+
+	
+	List<Book> findAll();
+	
+}
