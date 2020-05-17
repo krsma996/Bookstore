de00665eae5cbb720b0e6ae9dedf0f24ea74240c
NikolaKrsmanovic1996 <nikolakrsmanovic223@gmail.com>
2020-05-17 16:50:35 +0200

    ADDED NEW FEATURES
    Created CartItemService(repository and implementation)
    -created logic behind them
    
    Created ShoppingCartService(repository and implementation)
    -created logic behind them

---
+++ Bookstore/src/main/java/com/bookstore/service/ShoppingCartService.java
@@ -0,0 +1,10 @@
+package com.bookstore.service;
+
+import com.bookstore.domain.ShoppingCart;
+
+public interface ShoppingCartService {
+
+	void updateShoppingCart(ShoppingCart shoppingCart);
+	
+	
+}
